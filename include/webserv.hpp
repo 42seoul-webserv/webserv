053cb785a5d483da3554e534251e5ee35eefb0ce
Photogrammer <khsj15@khu.ac.kr>
2022-11-11 19:13:16 +0900

    branch #9

--- include/webserv.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef WEBSERV_HPP
- #define WEBSERV_HPP
-
-#include "GenericObject.hpp"
-#include <string>
-
-ObjectContainer* parseConfigure(char* configFilePath);
-ObjectContainer* parseObject(const std::string& fileData, size_t idx);
-#endif
