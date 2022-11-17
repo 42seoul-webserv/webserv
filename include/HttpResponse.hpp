f343da146f2b4dddabb87606c35b4dcf37840c7f
kimminkyeu <minkyeki@student.42seoul.kr>
2022-11-17 19:10:58 +0900

    HttpResponse Class

---
+++ include/HttpResponse.hpp
@@ -0,0 +1,98 @@
+#ifndef HTTP_RESPONSE_HPP
+#define HTTP_RESPONSE_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <ctime>
+#include <assert.h>
+
+/**----------------------
+ * * HeaderType         |
+ *----------------------*/
+class HeaderType {
+private:
+	static std::string GET_DAY(long tm_wday);
+	static std::string GET_MON(long tm_wmon);
+	static std::string getDate();
+
+public:
+  typedef std::pair<std::string, std::string> t_pair;
+
+  // 오늘 날짜를 계산해서 반환함.
+  static t_pair DATE(); // [ Date: ...today's date... ]
+  static t_pair SERVER(const std::string& server_name); // [ Server: server_name ]
+  static t_pair CONTENT_LENGTH(const size_t& len);
+  static t_pair CONTENT_LANGUAGE(const std::string& lan);
+  static t_pair CONTENT_TYPE(const std::string& type);
+
+  // HTTP/1.1 어플리케이션은 트랜잭션이 끝난 다음 커넥션을 끊으려면 Connection:close 헤더를 명시해야 한다.
+  // .. ex) Connection: Keep-Alive
+  static t_pair CONNECTION(const std::string& connection_mode);
+
+  // ex) Transfer-Encoding: gzip, chunked
+  // 만약 이 부분이 chunked라고 되어 있다면, content-length는 필요 없다.
+  static t_pair TRANSFER_ENCODING(const std::string& transfer_encoding_type);
+
+};
+
+/**----------------------
+ * * HttpResponseHeader |
+ *----------------------*/
+class HttpResponseHeader : public HeaderType {
+protected:
+	std::string _version;                            // HTTP/1.1
+	int _status_code;                                // 201
+	std::string _status_messege;                     // OK
+	std::map<std::string, std::string> _description; // other datas...
+
+public:
+	typedef std::map<std::string, std::string>::const_iterator t_iterator;
+
+	HttpResponseHeader();
+	HttpResponseHeader(const std::string &version, const int &status_code, const std::string &status_messege);
+};
+
+/*
+ [ HttpResponse TODO ]
+ * (1) content-length일 경우 Connection: close를 해준다 (필수는 아님)
+ * (2) Transfer-Encoding: chunked 일 경우 content-length 헤더를 없애고 chunked 방식 전송을 진행한다.
+ */
+
+class HttpResponse : public HttpResponseHeader {
+
+private:
+	std::string	_body;
+
+public: // Constructor & Destructor
+	HttpResponse();
+	HttpResponse(const int& status_code, const std::string& status_message);
+
+public: // Setters
+	void setVersion(const std::string& version);
+	void setStatus(const int &status_code, const std::string &status_messege);
+	void setBody(const std::string& body);
+	void setBody(const char* file_path);
+
+	// set Body with given file located in [file_path]
+	// NOTE: this sets file's total length to header.
+	void setBodyandUpdateContentLength(const char* file_path);
+
+    // add Header via std::pair type argument
+	void addHeader(const std::pair<std::string, std::string>& description_pair);
+
+	// simplest version of addHeader
+	void addHeader(const std::string& key, const std::string& value);
+
+public: // Getters
+	std::string getVersion();
+	int	getStatusCode();
+	std::string getStatusMessege();
+	std::string getBody();
+
+public: // Interface Functions
+	std::string	toString() const;
+};
+
+#endif
---
+++ src/HttpResponse.cpp
@@ -0,0 +1,189 @@
+#include "HttpResponse.hpp"
+
+/**----------------------
+ * * HeaderType         |
+ *----------------------*/
+std::string HeaderType::GET_DAY(long tm_wday) {
+	switch (tm_wday)
+	{
+		case 0: return ("Sun"); break;
+		case 1: return ("Mon"); break;
+		case 2: return ("Tue"); break;
+		case 3: return ("Wed"); break;
+		case 4: return ("Thu"); break;
+		case 5: return ("Fri"); break;
+		case 6: return ("Sat"); break;
+		default: return ("null"); break;
+	}
+}
+
+std::string HeaderType::GET_MON(long tm_wmon) {
+	switch (tm_wmon)
+	{
+		case 0: return ("Jan"); break;
+		case 1: return ("Feb"); break;
+		case 2: return ("Mar"); break;
+		case 3: return ("Apr"); break;
+		case 4: return ("May"); break;
+		case 5: return ("Jun"); break;
+		case 6: return ("Jul"); break;
+		case 7: return ("Aug"); break;
+		case 8: return ("Sep"); break;
+		case 9: return ("Oct"); break;
+		case 10: return ("Nov"); break;
+		case 11: return ("Dec"); break;
+		default: return ("null"); break;
+	}
+}
+
+std::string HeaderType::getDate() {
+	time_t curTime = time(NULL); // get current time info
+	struct tm *pLocal = gmtime(&curTime); // convert to struct for easy use
+	if (pLocal == NULL)
+	{
+		// ...
+		return ("null");
+	}
+	std::string result;
+	result = GET_DAY(pLocal->tm_wday) + ", " \
+					+ std::to_string(pLocal->tm_mday) + " " \
+					+ GET_MON(pLocal->tm_mon) + " " \
+					+ std::to_string(pLocal->tm_year + 1900) + " GMT";
+	return (result);
+}
+
+HeaderType::t_pair HeaderType::CONTENT_LENGTH(const size_t &len) {
+	return (std::pair<std::string, std::string>("Content-Length", std::to_string(len)));
+}
+
+HeaderType::t_pair HeaderType::CONTENT_LANGUAGE(const std::string &lan) {
+	return (std::pair<std::string, std::string>("Content-Language", lan));
+}
+
+HeaderType::t_pair HeaderType::CONTENT_TYPE(const std::string &type) {
+	return (std::pair<std::string, std::string>("Content-type", type));
+}
+
+HeaderType::t_pair HeaderType::DATE() {
+	return (std::pair<std::string, std::string>("Date", getDate()));
+}
+
+HeaderType::t_pair HeaderType::SERVER(const std::string &server_name) {
+	return (std::pair<std::string, std::string>("Sever", server_name));
+}
+
+HeaderType::t_pair HeaderType::CONNECTION(const std::string &connection_mode) {
+  return (std::pair<std::string, std::string>("Connection", connection_mode));
+}
+
+HeaderType::t_pair HeaderType::TRANSFER_ENCODING(const std::string &transfer_encoding_type) {
+  return (std::pair<std::string, std::string>("Transfer-Encoding", transfer_encoding_type));
+}
+
+/**----------------------
+ * * HttpResponseHeader |
+ *----------------------*/
+HttpResponseHeader::HttpResponseHeader()
+		: _version("HTTP/1.1"), _status_code(-1), _status_messege("null")
+{}
+
+HttpResponseHeader::HttpResponseHeader(const std::string &version, const int &status_code,
+									   const std::string &status_messege)
+		: _version(version), _status_code(status_code), _status_messege(status_messege)
+{}
+
+
+/**----------------------
+ * * HttpResponse       |
+ *----------------------*/
+HttpResponse::HttpResponse() // defaults to [ HTTP/1.1 | -1 | null ]
+		:HttpResponseHeader()
+{}
+
+HttpResponse::HttpResponse(const int &status_code, const std::string &status_message)
+		:HttpResponseHeader()
+{
+	_status_code = status_code;
+	_status_messege = status_message;
+}
+
+void HttpResponse::setVersion(const std::string &version) // ex. HTTP/1.1
+{
+	this->_version = version;
+}
+
+void HttpResponse::setStatus(const int &status_code, const std::string &status_messege) // ex. 404 PageNotFound
+{
+	this->_status_code = status_code;
+	this->_status_messege = status_messege;
+}
+
+void HttpResponse::setBody(const std::string &body) {
+	this->_body = body;
+}
+
+void HttpResponse::setBody(const char* file_path)
+{
+	std::ifstream file(file_path);
+	if (file.fail())
+	{
+		throw std::runtime_error("File Open failed\n");
+	}
+	std::string str;
+	std::string total_read;
+	while (getline(file, str))
+	{
+		_body += (str + "\n");
+	}
+}
+
+void HttpResponse::setBodyandUpdateContentLength(const char *file_path) {
+	setBody(file_path);
+	addHeader(HeaderType::CONTENT_LENGTH(_body.size()));
+}
+
+void HttpResponse::addHeader(const std::pair<std::string, std::string> &description_pair) {
+	this->_description[description_pair.first] = description_pair.second;
+}
+
+void HttpResponse::addHeader(const std::string &key, const std::string &value) {
+	this->_description[key] = value;
+}
+
+std::string HttpResponse::getVersion() { return this->_version; }
+
+int HttpResponse::getStatusCode() { return this->_status_code; }
+
+std::string HttpResponse::getStatusMessege() { return this->_status_messege; }
+
+std::string HttpResponse::getBody() { return this->_body; }
+
+std::string HttpResponse::toString() const
+{
+	if (_status_code < 10 && _status_code > 599)
+		throw std::runtime_error("Status Code:" + std::to_string(_status_code) + " -> HttpResponse::toString() : status code is out of range\n");
+	if (_status_messege == "null")
+		throw std::runtime_error("HttpResponse::toString() : status messege is not set\n");
+	// if has "content-length", then there should be no "chunked" option
+	{
+		bool has_content_length = false;
+		if (_description.find("Content-Length") != _description.end())
+			has_content_length = true;
+		bool has_option_chunked = false;
+		HttpResponseHeader::t_iterator itr = _description.find("Transfer-Encoding");
+		if (itr != _description.end() && itr->second.find("chunked") != std::string::npos)
+			has_option_chunked = true;
+		if (has_content_length + has_option_chunked != 1)
+			throw std::runtime_error("HttpResponse header has [chunked] and [content-length] value\n");
+	}
+	std::string message = _version + " " + std::to_string(_status_code) + " " + _status_messege + "\r\n";
+	HttpResponseHeader::t_iterator itr = _description.begin();
+	while (itr != _description.end())
+	{
+		message += (itr->first + ": " + itr->second + "\r\n");
+		itr++;
+	}
+	message += ("\n" + _body);
+
+	return (message);
+}
