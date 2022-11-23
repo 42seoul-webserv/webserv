83ddcb950f6dd39ddd7022bb5312c53b075e39b7
kimminkyeu <minkyeki@student.42seoul.kr>
2022-11-23 15:41:59 +0900

    Response Logic finished, need testing

--- src/RequestProcessor.cpp
+++ src/RequestProcessor.cpp
@@ -6,59 +6,6 @@
 
 static bool isAllowedMethod(std::vector<MethodType>& allowMethods, MethodType method)
 {
-<<<<<<< HEAD
-  /* 
-  * ! TODO: 
-  *   (1). location path searching alogirithm.
-  *   (2). Request Data와 처리 상황에 따라 적절한 response code 설정.
-  ?   (3).  큰 이미지파일일 경우 chunked로 처리 (?)
-  // *   (4). 생성자에서 default response 설정.
-  ?   (5). 큰 파일에 대한 read/write도 kevent를 통해 처리 (?)
-  */ 
-
-
-  // (1) init request object
-  HTTPResponse response(context);
-
-  // (2) set requested file to body (이 read부분 또한 kevent를 통해 handling?)
-  response.addHeader(HTTPResponse::CONTENT_LANGUAGE("en-US"));
-  response.addHeader(HTTPResponse::CONTENT_TYPE("text/html"));
-  // response.setBodyandUpdateContentLength("../index.html");
-  // std::string res = response.toString();
-
-  // (3) send string-converted Response Data to client.
-  // if (send(context->fd, res.data(), res.size(), 0) < 0)
-  // {
-    // printLog("error: client: " + getClientIP(&(context->addr)) + " : send failed\n", PRINT_RED);
-    // throw(std::runtime_error("Send Failed\n"));
-  // }
-  // printLog(getClientIP(&context->addr) + " send response\n", PRINT_BLUE);
-  // close(context->fd);
-}
-
-void RequestProcessor::processPOSTMethod(const HttpRequest &req,
-                                                 struct Context *context)
-{
-
-}
-
-void RequestProcessor::processPUTMethod(const HttpRequest &req,
-                                                struct Context *context)
-{
-
-}
-
-void RequestProcessor::processDELETEMethod(const HttpRequest &req,
-                                                   struct Context *context)
-{
-
-}
-
-void RequestProcessor::processHEADMethod(const HttpRequest &req,
-                                                 struct Context *context)
-{
-
-=======
   for (
           std::vector<MethodType>::iterator it = allowMethods.begin();
           it != allowMethods.end();
@@ -69,7 +16,6 @@ void RequestProcessor::processHEADMethod(const HttpRequest &req,
       return (true);
   }
   return (false);
->>>>>>> upstream/develop
 }
 
 // ASSUMPTION : request contain complete header...
