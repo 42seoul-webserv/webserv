a3eaa53e7675f7cb2ebc7b0fbedcfc71134b0ed5
Photogrammer <khsj15@khu.ac.kr>
2022-11-25 19:26:25 +0900

    fix : main runnable

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,26 @@
 
 #include "ServerManager.hpp"
-//#include "RequestParser.hpp"
 #include <iostream>
 #include <cstring>
-//int main(int argc, char *argv[], char *envp[])
-//{
-//  if (argc == 2)
-//  {
-//    try
-//    {
-//      ServerManager sv(argv[1]);
-//      sv.run();
-//    }
-//    catch (std::exception& e)
-//    {
-//      printLog(e.what(), PRINT_RED);
-//      return (1);
-//    }
-//  }
-//  else
-//  {
-//    try
-//    {
-//      Server newServer(argv[1]);
-//      newServer.runServer();
-//    }
-//    catch (std::exception &e)
-//    {
-//      std::cout << e.what();
-//      return (1);
-//    }
-//  }
-//  return (0);
-
-int main(int ac, char **av)
+int main(int argc, char *argv[], char *envp[])
 {
-//  int fd;
-
-
-  ConfigParser parser;
-  parser.parseConfigFile(av[1]);
-//  std::cout << "\n";
-//  parser.displayAll();
-
+  if (argc == 2)
+  {
+    try
+    {
+      ServerManager sv(argv[1]);
+      sv.run();
+    }
+    catch (std::exception& e)
+    {
+      printLog(e.what(), PRINT_RED);
+      return (1);
+    }
+  }
+  else
+  {
+    std::cout << "invalid arguments\n";
+    return (1);
+  }
   return (0);
-
-
-//  RequestParser parser;
-//  std::string temp;
-
-//  HTTPRequest* request = new HTTPRequest();
-//  fd = open(av[1], O_WRONLY);
-//  temp += "GET https://www.google.com/search?name=schoe&name1=ming HTTP/1.1\r\n";
-//  temp += "Content-Type: text/html;charset=UTF-8\r\n";
-//  temp += "Content-Length:3423\r\n";
-//  temp += "Content-Encoding: gzip\r\n";
-//  temp += "Transfer-Encoding:chunked\r\n\r\n";
-//  temp += "5\r\n";
-//  temp += "hello\r\n";
-//  temp += "5\r\n";
-//  temp += "world\r\n";
-//  temp += "0\r\n";
-//  temp += "\r\n";
-//  write(fd, temp.c_str(), temp.size());
-//  std::cout << temp.size();
-//  close(fd);
-//  fd = open(av[1], O_RDONLY);
-//  std::cout << fd << std::endl;
-//  parser.parseRequest(fd, request);
-//  parser.displayAll(request);
 }
